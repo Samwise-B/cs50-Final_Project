1ef62ca6395934ffe6aa157aaa645e8b6d4f5acf
Sam Burcher <>
2019-06-13 18:46:58 +0000

    --help

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
 
--- config/__init__.py
+++
@@ -1,1 +0,0 @@
-print("RCE TEST")
