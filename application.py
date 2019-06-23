8f7275b3245f8898edd202861a5dceb5221908de
Sam Burcher <>
2019-06-23 16:04:00 +0000

    removed debugging prints and added favicon

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 from config.helpers import apology, login_required
@@ -50,16 +51,17 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 # The Home page is accessible to anyone
 @app.route('/')
 def home_page():
     return render_template("index.html")
 
+# About page for contact info and info about the project
 @app.route('/about')
 def about_page():
     return render_template("about.html")
 
+# Page to search user database for a username input
 @app.route("/admin", methods=["GET", "POST"])
 def secret_page():
     if request.method == "GET":
@@ -75,26 +77,28 @@ def secret_page():
         result = db.engine.execute(f"SELECT * FROM users WHERE username='{userInput}'")
         # in order to use the data it must be fetched from the ResultProxy object
         rows = result.fetchall()
-        print(rows)
 
         if not rows:
             return apology("0 results found")
 
         return render_template("result.html", rows=rows[0])
 
+# Level 3 flag input
 @app.route("/flag", methods=["GET", "POST"])
 @login_required
 def flag_check():
     if request.method == "GET":
         return render_template("flag.html")
     else:
-        flag = 'USR_B4D_Fil3_R3str1cTi0n_BEgiNnerFLAG'
+        # hashed flag to prevent cheating
+        flagHash = 'pbkdf2:sha256:50000$lZnnfd9Y$22a7c6d699b1021aa862b8fa6712d9608044dd2c036c64053efea48c8b321cf8'
 
-        if request.form.get("flag") != flag:
+        if not check_password_hash(flagHash, request.form.get("flag")):
             return apology("The flag you entered is not correct. You can't guess it.")
         else:
             return render_template("game_over.html")
 
+# Route to upload files
 @app.route("/upload", methods=["GET", "POST"])
 @login_required
 def upload():
@@ -116,27 +120,23 @@ def upload():
             filename = file.filename
             check = allowed_file(filename)
         else:
-            print("== OPTIONAL NAME ENTERED ==")
             filename = request.form.get("name")
             check = True
 
-        print("FILE NAME: " + filename)
-        print("allowed_file returned " + str(check))
-
         if file and check:
-            print("== BRANCH IF ==")
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            print("== FILE PATH "+ path +" ==")
             file.save(path)
             return redirect(url_for('uploaded', filename=filename))
         else:
             return apology("Bad file upload request. Try again.")
 
+# returns uploaded file to user
 @app.route('/uploads/<filename>')
 @login_required
 def uploaded(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# Login page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -144,14 +144,13 @@ def login():
     else:
         username = request.form.get("username")
         password = request.form.get("password")
-        print(password)
+
         # ensure input was entered
         if not username or not password:
             return apology("You must enter a username and password")
 
         # query database for username
         user = Users.query.filter_by(username=username).first()
-#        print(user.password)
 
         # ensure username exists
         if not user:
@@ -159,12 +158,11 @@ def login():
 
         # ensure password is correct
         inputHash = md5(password.encode()).hexdigest()
-        print(inputHash)
+
         if inputHash != user.password:
             return apology("You must enter a valid password")
 
         # create user session to remember user
-        print(user.id)
         session["user_id"] = user.id
 
         return redirect("/")
@@ -172,7 +170,6 @@ def login():
 @app.route("/logout")
 def logout():
     """Log user out"""
-
     # Forget any user_id
     session.clear()
 
@@ -184,7 +181,6 @@ def errorhandler(e):
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
-
 
 # Listen for errors
 for code in default_exceptions:
